808bfe267d9e2ee173745b2a8286bb3275fde984
하이미디어학원 <@DESKTOP-AGC583K>
2024-03-27 18:01:48 +0900

    d

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -2,6 +2,7 @@ package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
     
+	@Override
     public void execute(String str){
             
        if("앱".equals(str)){
@@ -9,26 +10,23 @@ public class SmartPhone extends MusicPhone {
        }else if ("음악".equals(str)){
     	   playMusic();
        } else {   
-    	   super.execute(str);  
-    	  
-}
+    	   super.execute(str);   
+       }
                 
        
   }
         
-
- 
     //메소드작성
-protected void playApp(){
+	protected void playApp(){
     System.out.println("앱실행");
-}
+	}
 
 
-    
     //메소드작성
-protected void playMusic(){
+	@Override
+	protected void playMusic(){
     System.out.println("다운로드해서 음악재생");
-}
+	}
     
 }
 
