1960477b7ad61bfd33419ca36c6c7cb9d20a18c4
하이미디어학원 <@DESKTOP-AGC583K>
2024-03-27 14:19:39 +0900

    d

---
+++ src/com/javaex/ex01/Customer.java
@@ -0,0 +1,34 @@
+package com.javaex.ex01;
+
+public class Customer extends Person{
+	private int cNo; 
+	private int point; 
+			
+	public Customer (String name, String hp, int cNo, int point) {
+		super(name, hp);
+		this.cNo = cNo;
+		this.point = point;
+		
+	}
+	
+	public int getCno() {
+
+		return cNo;
+	}
+	public void setCno(int cNo) {
+		this.cNo = cNo;
+	}
+	public int getPoint() {
+
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	@Override
+	public void showInfo() {
+		System.out.println("#이름:" + name + " #핸드폰:" + hp + " #고객번호:" + cNo + " #포인트:" + point);
+	}
+	
+}
